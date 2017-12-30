90da2f664bdd128c96cbfa7ab956ab34ad2c9dac
Thomas Oelke Adriano <thomas@thomas-mpb.local>
2017-12-30 16:02:07 -0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <header>
+      <span>Vue.js PWA</span>
+    </header>
+    <main>
+      <img src="./assets/logo.png" alt="Vue.js PWA">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  mounted() {
+    this.$nextTick(() => {
+      // chamar o mdc dentro do nextTick garante que todos os filhos de <app/> já estarão presentes na DOM
+      /* global mdc */
+      mdc.autoInit();
+    });
+  }
+};
+</script>
+
+<style lang="scss">
+$appColor: #2c3e50;
+$navbarColor: #35495e;
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: $appColor;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  margin: 0;
+  height: 56px;
+  padding: 0 16px 0 24px;
+  background-color: $navbarColor;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: 0.02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 16px;
+}
+</style>
